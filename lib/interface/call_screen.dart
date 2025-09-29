94ebcc4b923e74eaacd9cb22da85aa766f277d6d
Abdullah Mahmood <139673550+abdullah5341@users.noreply.github.com>
2025-09-29 18:27:05 +0500

    login screen in between

--- lib/interface/call_screen.dart
+++ lib/interface/call_screen.dart
@@ -1,83 +1,44 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-class CallScreen extends StatefulWidget {
+class CallScreen extends StatelessWidget {
   const CallScreen({super.key});
 
   @override
-  State<CallScreen> createState() => _CallScreenState();
-}
-
-class _CallScreenState extends State<CallScreen> {
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        // appBar: AppBar(),
-        body: Center(
-      child: Container(
-        height: 250,
-        width: 250,
-        padding: const EdgeInsets.all(40),
-        decoration: BoxDecoration(
-          color: const Color.fromARGB(255, 210, 240, 247),
-          borderRadius: BorderRadius.circular(30),
-        ),
-        child: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "El Cairo",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-                  ),
-                  Text(
-                    "4.5",
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16,
-                        color: Colors.amber),
-                  )
-                ],
-              ),
-              const Text(
-                "Egypt",
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-              ),
-              const Center(
-                child: Text(
-                  "Image",
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    height: 40,
-                    width: 70,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                    child: Center(child: Text("More")),
-                  ),
-                  Container(
-                    height: 40,
-                    width: 70,
-                    decoration: BoxDecoration(
-                      color: const Color.fromARGB(255, 43, 114, 236),
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                    child: Center(child: Text("\$100")),
-                  )
-                ],
-              )
-            ],
+        backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Text(
+            "Login Page",
+            style: const TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              color: Color.fromARGB(255, 240, 236, 236),
+            ),
           ),
         ),
-      ),
-    ));
+        body: Center(
+          child: Container(
+            height: 550,
+            width: 1000,
+            padding: const EdgeInsets.all(150),
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 144, 223, 212),
+            ),
+            child: Column(
+              children: [
+                Text(
+                  "FLUTTER",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
